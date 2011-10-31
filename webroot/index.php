08bb12618d3ad3335d10409fbe48c73b794ca871
gwutama <galuh.utama@gwutama.de>
2011-10-31 10:08:45 +0100

    PHP_SELF warning in webroot/index.php fixed

--- webroot/index.php
+++ webroot/index.php
@@ -5,7 +5,7 @@ include "../libs/core.php";
 define("DEFAULT_CONTROLLER", "Home");
 define("DEFAULT_ACTION", "index");
 define("DS", DIRECTORY_SEPARATOR);
-define("RELATIVE_URL", str_replace("/webroot/index.php", "", $_SERVER[PHP_SELF]));
+define("RELATIVE_URL", str_replace("/webroot/index.php", "", $_SERVER['PHP_SELF']));
 
 try {
     Route::dispatch(@$_GET["url"], DEFAULT_CONTROLLER, DEFAULT_ACTION);
