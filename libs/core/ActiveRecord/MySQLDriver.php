7bcf3933cfb197598692ea28a997122eea757391
gwutama <galuh.utama@gwutama.de>
2012-01-29 19:48:01 +0100

    Couldn't remove unnecessary file

---
+++ libs/core/ActiveRecord/MySQLDriver.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Galuh Utama
+ * Date: 1/29/12
+ * Time: 6:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class MySQLDriver
+{
+
+}
