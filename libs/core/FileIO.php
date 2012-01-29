cf2a96f6046a2484da75837a2e1f2f5a76d86271
Galuh Utama <galuh.utama@gwutama.de>
2012-01-29 02:11:35 +0100

    Class FileIO added.

---
+++ libs/core/FileIO.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * <h1>Class FileIO</h1>
+ *
+ * <p>
+ * This class contains file IO operations.
+ * </p>
+ */
+class FileIO
+{
+    /**
+     * Writes data to a file.
+     *
+     * @static
+     * @param $file     Filename.
+     * @param $data     Data to write.
+     */
+    public static function write($file, $data) {
+        $fh = fopen($file, 'w+');
+        fwrite($fh, $data);
+        fclose($fh);
+    }
+
+
+    /**
+     * Reads data from a file.
+     *
+     * @static
+     * @param $file     Filename.
+     * @return string
+     */
+    public static function read($file) {
+        $fh = fopen($file, 'r');
+        $data = fread($fh, filesize($file));
+        fclose($fh);
+
+        return $data;
+    }
+
+
+    /**
+     * Deletes a specific file.
+     *
+     * @static
+     * @param $file     Filename.
+     * @throws exception
+     */
+    public static function delete($file) {
+        if (!file_exists($file)) {
+            throw new exception("File $file doesn't exist.");
+        }
+        else {
+            @unlink($file);
+        }
+    }
+
+}
+
+?>
