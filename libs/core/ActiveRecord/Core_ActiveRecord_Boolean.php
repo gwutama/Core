76d75a42fe6b44725f8bcd3cc365b8435f9d83ac
gwutama <galuh.utama@gwutama.de>
2012-01-30 20:32:40 +0100

    Unnecessary IDE files deleted.

---
+++ libs/core/ActiveRecord/Core_ActiveRecord_Boolean.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Galuh Utama
+ * Date: 1/30/12
+ * Time: 5:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Core_ActiveRecord_Boolean
+{
+
+}
---
+++ libs/core/ActiveRecord/Core_ActiveRecord_Comparator.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Galuh Utama
+ * Date: 1/30/12
+ * Time: 5:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Core_ActiveRecord_Comparator
+{
+
+}
